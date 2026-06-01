b24f70aa0c5104fee2c4a28b35d176648b3b72e7
Duggasani2002 <128168296+Duggasani2002@users.noreply.github.com>
2026-06-01 10:17:54 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+        var i = 0
+        var j = s.length - 1
+
+        while (i < j) {
+            while (i < j && !s[i].isLetterOrDigit()) i++
+            while (i < j && !s[j].isLetterOrDigit()) j--
+
+            if (s[i].lowercaseChar() != s[j].lowercaseChar()) {
+                return false
+            }
+
+            i++
+            j--
+        }
+
+        return true
+    }
+}
