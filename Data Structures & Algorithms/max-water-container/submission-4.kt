4a60bbf9a4ba3c9b4fe6cbccc4c436602150a0d1
Duggasani2002 <128168296+Duggasani2002@users.noreply.github.com>
2026-06-12 23:15:05 +0530

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun maxArea(heights: IntArray): Int {
+        var l = 0
+        var r = heights.size - 1
+        var maxArea = 0
+        while (l < r) {
+            val area = minOf(heights[l], heights[r]) * (r - l)
+            maxArea = maxOf(maxArea, area)
+            // Move the pointer of the shorter height
+            if (heights[l] < heights[r]) l++ else r--
+        }
+        return maxArea
+    }
+}
