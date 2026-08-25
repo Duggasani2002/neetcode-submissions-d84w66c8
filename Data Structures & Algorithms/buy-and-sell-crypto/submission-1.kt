805dccc396b1c067bc85cc0a4ee0ad78f22d2100
Duggasani2002 <128168296+Duggasani2002@users.noreply.github.com>
2026-08-25 15:29:20 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun maxProfit(prices: IntArray): Int {
+        var buy = prices[0]
+        var maxProfit = 0
+        for(amount in prices) {
+            if(amount < buy) {
+                buy = amount
+            }  else {
+                maxProfit = maxOf(maxProfit, amount-buy)
+            }
+        }
+
+        return maxProfit
+    }
+}
